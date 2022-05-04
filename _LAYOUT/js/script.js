d9b64c6c081ce4888acfa4d42dfa427e4d0f1b9b
Zac <a19860101@gmail.com>
2022-05-04 21:17:21 +0800

    20220504

--- _LAYOUT/js/script.js
+++ _LAYOUT/js/script.js
@@ -3,13 +3,33 @@ window.onload = function () {
     let navToggle = document.querySelector(".nav-toggle");
 
     console.log(navToggle);
-    navToggle.onclick = e => {
-        let navGroup = document.querySelector('.nav-group');
-        navGroup.classList.toggle('open');
-        if(navGroup.style.height){
+    navToggle.onclick = (e) => {
+        let navGroup = document.querySelector(".nav-group");
+        navGroup.classList.toggle("open");
+        if (navGroup.style.height) {
             navGroup.style.height = null;
-        }else{
-            navGroup.style.height = navGroup.scrollHeight + 'px';
+        } else {
+            navGroup.style.height = navGroup.scrollHeight + "px";
         }
+    };
+
+    // modal
+    let modalBtns = document.querySelectorAll(".modal-btn");
+    for (modalBtn of modalBtns) {
+        modalBtn.onclick = (e) => {
+            // let modal = e.target.dataset.modal;
+            let modal = e.target.getAttribute('href');
+            document.querySelector(modal).classList.add("active");
+            e.preventDefault();
+            return false;
+        };
     }
+
+    document.body.onclick = (e) => {
+        // console.log(e.target.className);
+        // console.log(e.target.classList);
+        if (e.target.className.indexOf("modal-close") > -1) {
+            document.querySelector(".modal.active").classList.remove("active");
+        }
+    };
 };
